b79db7de4885687343a2c47592b8ed193dd3ced5
camain-elie <camain.elie@gmail.com>
2021-05-29 14:11:25 +0200

    Added genuine weather data through API call. Fixed display bug due to too much precision in the datas. Added user geolocalisation.

--- src/App.js
+++ src/App.js
@@ -8,50 +8,107 @@ import { Forecast, toRetardUnit } from './components/forecast';
 
 import './App.scss';
 
+const getUserWoeid = async (latt, long) => {
+  const response = await fetch(`https://api.allorigins.win/raw?url=https://www.metaweather.com/api/location/search/?lattlong=${latt},${long}`);
+  const jsonData = await response.json();
+  return jsonData;
+}
+
+const getWheatherDatas = async (location) => {
+  const response = await fetch(`https://api.allorigins.win/raw?url=https://www.metaweather.com/api/location/${location}`)
+  const jsonData = await response.json();
+  return jsonData;
+}
+
 class App extends Component {
   constructor(props){
     super(props);
 
+    this.handlePositionClick = this.handlePositionClick.bind(this);
+
     this.state={
-      location: 'Paris',
+      location: 'Orléans',
       metric: true,
-    }
+      isLoading: true,
+      userMessage: '',
+
+    };
   }
 
   switchUnit(){
     this.setState({ metric: !this.state.metric});
   }
 
+  setData(data){
+    console.log(data);
+    this.setState({
+      weatherData: data.consolidated_weather,
+      location: data.title,
+      today: data.consolidated_weather[0],
+      isLoading: false,
+    })
+  }
 
+  handlePositionClick(){
+    console.log('position click');
+    this.useUserPosition();
+  }
+
+  useUserPosition () {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        getUserWoeid(position.coords.latitude, position.coords.longitude)
+          .then(woeid => getWheatherDatas(woeid[0].woeid)
+            .then(data => this.setData(data)))
+      },
+      (error) => this.setState({ userMessage: error.message }));
+  }
+
+  handleSearchForPlacesClick(){
+
+  }
+
+  componentDidMount(){
+    this.useUserPosition();
+    /*getUserWhoid(28,238)
+    .then(res => console.log(res))
+    getWheatherDatas(9807)
+    .then(res => console.log(res))*/
+  }
 
   render(){
 
     const state = this.state;
 
     return(
+      !state.isLoading &&
       <div className="app">
         <div className="today">
           <div className="today__buttons">
             <p className="today__search">
               Search for places
             </p>
-            <div className="today__position">
+            <div className="today__position"
+              onClick={() => this.handlePositionClick()}>
               <p className="material-icons">gps_fixed</p>
             </div>
           </div>
 
           <div className="today__icon">
-            <WeatherIcon abbreviation="s" />
+            <WeatherIcon abbreviation={state.today.weather_state_abbr} />
             <div></div>
           </div>
 
-          <p className="today__temp">{state.metric ? '15' : toRetardUnit(15) }<span className="today__temp-unit">{state.metric ? '°C' : '°F'}</span></p>
-
-          <p className="today__weather">Shower</p>
-
+          <p className="today__temp">
+            {state.metric ? Math.round(state.today.the_temp) : toRetardUnit(Math.round(state.today.the_temp)) }
+            <span className="today__temp-unit">
+              {state.metric ? '°C' : '°F'}
+            </span>
+          </p>
+          <p className="today__weather">{state.today.weather_state_name}</p>
           <p className="today__date">Today - {(new Date()).toUTCString().substring(0, 11)}</p>
-
-          <p className="today__location"><span className="today__location-icon material-icons">place</span> {state.location}</p>
+          <p className="today__location">
+            <span className="today__location-icon material-icons">place</span> {state.location}</p>
         </div>
 
         <div className="content">
@@ -63,23 +120,27 @@ class App extends Component {
           </div>
 
           <div className="forecasts">
-            <Forecast metric={state.metric} date={1} high={6} low={1} abbr='hr' />
-            <Forecast metric={state.metric} date={2} high={11} low={6} abbr='lc' />
-            <Forecast metric={state.metric} date={3} high={18} low={12} />
-            <Forecast metric={state.metric} date={4} high={21} low={11} />
-            <Forecast metric={state.metric} date={5} high={20} low={12} abbr='s' />
+            {state.weatherData.map(
+              (item, index) => {
+                if(index > 0 && index < 6){
+                  return <Forecast key={index} metric={state.metric}  
+                    date={index} high={Math.round(item.max_temp)}
+                    low={Math.round(item.min_temp)} abbr={item.weather_state_abbr} />;
+              }
+              return null;
+            })}
           </div>
 
           <h2>Today's highlights</h2>
           <div className="highlights">
             
 
-            <Highlight title="Wind status" value={7} metric={state.metric}
-            unit={state.metric ? 'km/h' : 'mph'} element={<Wind direction="WSW" />} />
-            <Humidity value={82} />
-            <Highlight title="Visibility" value={7} metric={state.metric}
+            <Highlight title="Wind status" value={state.today.wind_speed} metric={state.metric}
+            unit={state.metric ? 'km/h' : 'mph'} element={<Wind direction={state.today.wind_direction_compass} />} />
+            <Humidity value={state.today.humidity} />
+            <Highlight title="Visibility" value={state.today.visibility} metric={state.metric}
               unit={state.metric ? 'km' : 'miles'} />
-            <Highlight title="Air Pressure" value={998} unit="mb" />
+            <Highlight title="Air Pressure" value={state.today.air_pressure} unit="mb" />
           </div>
         </div>
       </div>
